853cffc0b9ca98373ab5906d5c90db3424783daa
DALEJ-008 <dnaljpll@gmail.com>
2025-10-16 18:46:09 -0500

    Cambios reportes

--- Backend/management/commands/test_twilio.py
+++
@@ -1,8 +0,0 @@
-from django.core.management.base import BaseCommand
-
-
-class Command(BaseCommand):
-    help = 'Twilio integration removed; this command is a no-op.'
-
-    def handle(self, *args, **options):
-        self.stdout.write(self.style.WARNING('Twilio integration has been removed from this project.'))
--- scripts/add_foto_column.py
+++
@@ -1,8 +0,0 @@
-import os
-os.environ.setdefault('DJANGO_SETTINGS_MODULE','secuseo_project.settings')
-from django import setup
-setup()
-from django.db import connection
-with connection.cursor() as cur:
-    cur.execute('ALTER TABLE "Backend_userprofile" ADD COLUMN IF NOT EXISTS foto varchar(512)')
-print('ALTER OK')
